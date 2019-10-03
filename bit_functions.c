265002bdc437af7dd204a6678ea4d90b3aacddb1
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-10-02 20:41:12 -0400

    modified bit functions to be up to spec w/ assign

--- bit_functions.c
+++ bit_functions.c
@@ -1,12 +1,17 @@
-#include "bit_functions.h"
+#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
 
 
+/**
+ * find the fisrt bit set in value, beginning with bit position 0
+ * @param value - the value to search for a bit set
+ * @return i - the first bit position or -1 if the value is 0
+ */
 
 int bit_find(int value){
 
-    for (int i = 0; i < 32; i++){
+    for (int i = 0; i < 32; i++)
         if (CHECK_BIT(value, i))
             return i;
-    }
-
+    return -1;
 }
+
