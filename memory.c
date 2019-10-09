10aa641e37c026cf0ba79584a8ae0716be429a0d
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-10-09 14:01:23 -0400

    Edits made after talking to gusty

--- memory.c
+++ memory.c
@@ -7,30 +7,35 @@
  * */
 
 struct mem_chip{
-    
     unsigned char mem_arr[32][32];
 
 } mem_chip;
 
 
+
+
 unsigned char mem_get(unsigned int row_selected, unsigned int col_selected){
 
-    unsigned char mem;
-
+    
+    //Was at risk of reimplementing bit_find - so this is my solution
+    return mem_chip.mem_arr[bit_find(row_selected)][bit_find(col_selected)];
+    
+    /* Commented out b/c this is the job of bit_find() 
     //loop through 2d array and detect whether the char is the same from both ends
     for (int i = 0; i <= 32; i++){
         for (int j = 0 ; j <= 32 ; j++){
            if (mem_chip.mem_arr[i][j] == mem_chip.mem_arr[row_selected][col_selected]){
               mem = mem_chip.mem_arr[i][j]; //set it to mem 
            } 
-
         }
-    }
+    }*/
 
-	return mem; //return the unsigned char
 }
 
 void mem_put(unsigned int row_selected, unsigned int col_selected, unsigned char b){
+    //Set this spot in the array to the unsigned char b that was passed in 
+    mem_chip.mem_arr[bit_find(row_selected)][bit_find(col_selected)] = b;
+    
 
 
 }
