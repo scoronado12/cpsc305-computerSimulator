de09961c1abad34690f7e9834b68b8b9dbe5d1e1
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-10-10 18:42:25 -0400

    working on load_memory()

--- memory_system.c
+++ memory_system.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 #include "memory_system.h"
+
+void memory_store(int address, unsigned char value){
+
+
+}
+
+
+void load_memory(char *filename){
+    FILE *fp;
+    
+    fp = fopen(filename, "r");
+
+    
+    char data[20];
+    while(1){
+        if (fscanf(fp, "%d") == EOF){
+            break;
+        } else{
+
+            //TODO load into spot in memory?
+
+        }
+    }    
+
+}
