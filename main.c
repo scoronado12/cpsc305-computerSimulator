99bb21f2e4cdf7347ac1d92284a7260dac77eb67
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-10-24 10:50:52 -0400

    gusty approves

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(){
 	
     load_memory("mem_load.txt");
     memory_store(100, 10);
-    printf("memory_fetch: %d\n", memory_fetch(100)); 
+    printf("memory_fetch: %d \n", memory_fetch(100)); 
     //memory_dump(8, 8);
     memory_store_word(12, 0x0a0b0c0d); //store 0x0a0b0c0d in address 12
     
@@ -16,7 +16,11 @@ int main(){
     //memory_dump(8, 8);
     printf("memory_fetch_word(12): 0x%08x\n", memory_fetch_word(12)); //0x0a0b0c0d should print out
     
+    memory_store_word(12, 0xaabbccdd); 
+    printf("memory_fetch_word(12): 0x%08x\n", memory_fetch_word(12));
     
+    printf("memory_fetch: %x\n", memory_fetch(13)); 
+
     return 0;	 
 }
 
--- memory_system.c
+++ memory_system.c
@@ -9,20 +9,33 @@
 void memory_store(int address, unsigned char value){
     
     //mem_chip.mem_arr[MEM_ROW][MEM_COL] = decoder(value);
-    mem_put(MEM_ROW, MEM_COL, value);
+    unsigned int lower5 = (address) & 0x1f;
+    unsigned int upper5 = (address >> 5) & 0x1f;
+    
+    mem_put(decoder(lower5), decoder(upper5), value);
 }
 
 
 unsigned char memory_fetch(int address){
     
-    return mem_get(MEM_ROW, MEM_COL);
+    unsigned int lower5 = (address) & 0x1f;
+    unsigned int upper5 = (address >> 5) & 0x1f;
+    
+    return mem_get(decoder(lower5), decoder(upper5));
 
 }
 
 unsigned int memory_fetch_word(int address){
-    //address = decoder(address);
-    return decoder(mem_chip.mem_arr[MEM_ROW][MEM_COL]);
-   // return mem_chip.mem_arr[MEM_ROW][MEM_COL];
+    
+    unsigned byte1 = memory_fetch(address);
+    unsigned byte2 = memory_fetch(address + 1);
+    unsigned byte3 = memory_fetch(address + 2);
+    unsigned byte4 = memory_fetch(address + 3);
+    
+    
+    //reverse operation of memory store word
+    return (byte4 << 24) | (byte3 << 16) | (byte2 << 8) | byte1;
+    
 }
 
 void memory_dump(int start_address, int num_bytes){
@@ -44,6 +57,7 @@ void memory_store_word(int address, unsigned int value){
     unsigned byte3 = (value >> 16) & 0xff;
     unsigned byte4 = (value >> 24) & 0xff;
 
+    printf("%x %x %x %x\n",byte1, byte2, byte3, byte4);
     //store the memory
     memory_store(address, byte1);
     memory_store(address + 1, byte2);
@@ -57,19 +71,27 @@ void load_memory(char *filename){
     FILE *fp;
      
     fp = fopen(filename, "r");
+    unsigned int addr;
+    unsigned int data_buff;
+    fscanf(fp, "%x", &addr);
     
-    char data_buff[10];
+    
     while(1){
-        if (fscanf(fp, "%d", data_buff) == EOF){ //changed from %d to %x per gusty's instructions
+        if (fscanf(fp, "%x", &data_buff) == EOF){ //changed from %d to %x per gusty's instructions
             break;
         }
+        
+        memory_store_word(addr += 4, data_buff);
+        
     }    
+    
+    /*
     char add = data_buff[0];
     int starting_addr = (int) add;
 //      printf("Value of 'add': %d\n", starting_addr);
     for(int i = 1; i < 10; i++){
 //	printf("The value: %x\nThe address: %d\n", data_buff[i], starting_addr + (i-1));  
-	memory_store_word(starting_addr + (i-1), data_buff[i]);
+        memory_store_word(starting_addr + (i-1), data_buff[i]);
     }   
-
+*/
 }
