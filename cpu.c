7c704d0cf16d84d2160aed7565cd718112e76b18
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-10-26 14:22:38 -0400

    initial cpu commits

---
+++ cpu.c
@@ -0,0 +1,19 @@
+#include "bit_functions.h"
+int registers[16];
+int cpsr; //current prog status register
+
+void set_reg(int reg, int value){
+    
+}
+
+int get_reg(int reg){
+    
+}
+
+int get_cpsr(){
+
+}
+
+void step(){
+    
+}
