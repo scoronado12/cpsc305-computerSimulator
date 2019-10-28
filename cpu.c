2ceea6b30ae724d7a6a72b7ef7fd01ca0d0aa83f
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-10-28 13:10:56 -0400

    added switch statement

--- cpu.c
+++ cpu.c
@@ -3,17 +3,64 @@ int registers[16];
 int cpsr; //current prog status register
 
 void set_reg(int reg, int value){
-    
+  registers[reg] = value;  
 }
 
 int get_reg(int reg){
-    
+   return registers[reg]; 
 }
 
 int get_cpsr(){
-
+    return cpsr;
 }
 
 void step(){
-    
+   switch (opcode) {
+       case LDR:
+           reg = inst >> 16 & 0xff;
+           address = inst & 0xffff;
+           if (address > 1023 || reg > 15) {
+               printf("Address/Register out of bounds.\n");
+               exit(1);
+           }
+           registers[reg] = memory_fetch_word(address);
+           printf("reg: %d, reg val: 0x%x08x, address: 0x%04x\n", reg, registers[reg], address);
+
+           pc += 4;
+           break;
+       case STR:
+          //memory store word
+          break; 
+       case ADD:
+          //add
+          break;
+       case SUB:
+          //subtract
+          break;
+       case MUL:
+          //multiply
+          break;
+       case DIV:
+          //divide
+          break;
+       case CMP:
+          //compare
+       case B:
+          //branch
+          break;
+       case BEQ:
+          //branch equal to
+          break;
+       case BNE:
+         //branch not equal
+       case BLT:
+         //branch less than
+          break;
+
+       case BGT:
+          //branch greater than
+          break;
+
+   }
+ 
 }
